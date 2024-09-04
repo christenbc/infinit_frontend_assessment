b0b6efba8f54bd21fc13a9f6585dbe70e4026b27
Christen <christen.bc@gmail.com>
2024-09-04 11:12:47 +0200

    draft detailed view and simple navigation

--- lib/pages/detailed.dart
+++ lib/pages/detailed.dart
@@ -1,10 +1,115 @@
 import 'package:flutter/material.dart';
+import 'package:infinit_frontend_assessment/models/models.dart';
+
+const pagePadding = 24.0;
 
 class DetailedPage extends StatelessWidget {
-  const DetailedPage({super.key});
+  final Movie movie;
+
+  const DetailedPage({super.key, required this.movie});
 
   @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: Text(movie.title),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Image.network(movie.backdropImageThumb),
+              Padding(
+                padding: const EdgeInsets.all(pagePadding),
+                child: Text(movie.overview),
+              ),
+              Padding(
+                padding: getInnerElementPadding(),
+                child: RichText(
+                  textAlign: TextAlign.start,
+                  text: TextSpan(
+                    text: 'Release date: ',
+                    style: const TextStyle(color: Colors.black),
+                    children: <TextSpan>[
+                      TextSpan(
+                        text: movie.release_date,
+                        style: const TextStyle(
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: getInnerElementPadding(),
+                child: RichText(
+                  textAlign: TextAlign.start,
+                  text: TextSpan(
+                    text: 'Vote average: ',
+                    style: const TextStyle(color: Colors.black),
+                    children: <TextSpan>[
+                      TextSpan(
+                        text: movie.vote_average.toString(),
+                        style: const TextStyle(
+                          color: Colors.grey,
+                        ),
+                      ),
+                      const TextSpan(
+                        text: ' based on ',
+                      ),
+                      TextSpan(
+                        text: movie.vote_count.toString(),
+                        style: const TextStyle(
+                          color: Colors.grey,
+                        ),
+                      ),
+                      const TextSpan(
+                        text: ' votes',
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: getInnerElementPadding(),
+                child: RichText(
+                  textAlign: TextAlign.start,
+                  text: TextSpan(
+                    text: 'Popularity: ',
+                    style: const TextStyle(color: Colors.black),
+                    children: <TextSpan>[
+                      TextSpan(
+                        text: movie.popularity.toString(),
+                        style: const TextStyle(
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: getInnerElementPadding(),
+                child: RichText(
+                  textAlign: TextAlign.start,
+                  text: TextSpan(
+                    text: 'Original language: ',
+                    style: const TextStyle(color: Colors.black),
+                    children: <TextSpan>[
+                      TextSpan(
+                        text: movie.original_language,
+                        style: const TextStyle(
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+
+  EdgeInsets getInnerElementPadding() => const EdgeInsets.fromLTRB(pagePadding, 0, pagePadding, pagePadding);
 }
--- lib/pages/widgets/top_rated_list_view.dart
+++ lib/pages/widgets/top_rated_list_view.dart
@@ -1,34 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:infinit_frontend_assessment/blocs/blocs.dart';
+import 'package:infinit_frontend_assessment/pages/pages.dart';
 
 class TopRatedListView extends StatelessWidget {
   const TopRatedListView({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return BlocConsumer<MoviesCubit, MoviesState>(
-      listener: (context, state) {
-        // TODO: implement listener
-      },
-      builder: (context, state) => ListView.separated(
-        itemCount: state.topMovies.length,
-        itemBuilder: (context, index) {
-          final topMovie = state.topMovies[index];
-          return ListTile(
-            leading: AspectRatio(
-              aspectRatio: 1,
-              child: Image.network(
-                topMovie.posterImageThumb,
-                fit: BoxFit.cover,
-              ),
-            ),
-            title: Text(topMovie.title),
-            trailing: Text('⭐ ${topMovie.vote_average.toString()}'),
-          );
+  Widget build(BuildContext context) => BlocConsumer<MoviesCubit, MoviesState>(
+        listener: (context, state) {
+          // TODO: implement listener
         },
-        separatorBuilder: (context, index) => const SizedBox(height: 8),
-      ),
-    );
-  }
+        builder: (context, state) => ListView.separated(
+          itemCount: state.topMovies.length,
+          itemBuilder: (context, index) {
+            final topMovie = state.topMovies[index];
+            return ListTile(
+              onTap: () => Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => DetailedPage(movie: topMovie)),
+              ),
+              leading: AspectRatio(
+                aspectRatio: 1,
+                child: Image.network(
+                  topMovie.posterImageThumb,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              title: Text(topMovie.title),
+              trailing: Text('⭐ ${topMovie.vote_average.toString()}'),
+            );
+          },
+          separatorBuilder: (context, index) => const SizedBox(height: 8),
+        ),
+      );
 }
